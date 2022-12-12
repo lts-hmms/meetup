3c3a4ca849ab7917f69793e877432fb67b9ce0b2
lts-hmms <97221909+lts-hmms@users.noreply.github.com>
2022-12-12 10:30:54 +0100

    feat: add show/hide details

--- src/Event.js
+++ src/Event.js
@@ -1,15 +1,19 @@
 import React, { Component } from "react";
 
 
+
 class Event extends Component {
+    state = { collapsed: true }
+
     render(){
-        const { event } = this.props
+        const { event } = this.props;
+        const { collapsed } = this.state;
         return <div>
             <p className='title'>Learn JavaScript</p>
             <p className='date'>2020-05-19T16:00:00+02:00</p>
             <p className='time'>Europe/Berlin</p>
             <p className='place'>London, UK</p>
-            <button type='button' className='details-btn'>show details</button>
+            <button type='button' className='details-btn'>{collapsed ? 'Show details' : 'Hide Details'}</button>
 
         </div>
     }
