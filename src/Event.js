8d724ac7088af509fa96d4a8f45580a5a96747e0
lts-hmms <97221909+lts-hmms@users.noreply.github.com>
2022-12-12 13:23:24 +0100

    feat: add test show event-details

--- src/Event.js
+++ src/Event.js
@@ -13,16 +13,26 @@ class Event extends Component {
     render(){
         const { event } = this.props;
         const { collapsed } = this.state;
-        return <div>
+        return (<div className='event-overview'>
             <p className='title'>Learn JavaScript</p>
             <p className='date'>2020-05-19T16:00:00+02:00</p>
             <p className='time'>Europe/Berlin</p>
             <p className='place'>London, UK</p>
+            
+            {!collapsed && (
+                <div className='event-details'>
+                <p className='title'>Learn JavaScript</p>
+                <p className='date'>2020-05-19T16:00:00+02:00</p>
+                <p className='time'>Europe/Berlin</p>
+                <p className='place'>London, UK</p>
+                <div className='description'>Have you wondered how you can ask Google to show you the list of the top ten must-see places in London? And how Google presents you the list? How can you submit the details of an application? Well, JavaScript is doing these. :) \n\nJavascript offers interactivity to a dull, static website. Come, learn JavaScript with us and make those beautiful websites.</div>
+              </div >
+            )}
             <button type='button' className='details-btn' onClick={() => this.toggleDetails()}>
-                    {collapsed ? 'Show details' : 'Hide Details'}
+                    {collapsed ? 'Show details' : 'Hide details'}
             </button>
 
-        </div>
+        </div>)
     }
 }
 
--- src/__tests__/Event.test.js
+++ src/__tests__/Event.test.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import Event from '../Event';
 import { mockData } from '../mock-data';
+import { text } from 'stream/consumers';
 
 describe('<Event /> component', () => {
     let EventWrapper;
@@ -11,18 +12,15 @@ describe('<Event /> component', () => {
         EventWrapper = shallow(<Event/>)
     });
 
-    /**
-     * Feature 2, Scenario 1: An event element is collapsed by default
-     */
-test('render event title', () => {
-    expect(EventWrapper.find('.title')).toHaveLength(1);
-});
+/**
+* Feature 2, Scenario 1: An event element is collapsed by default
+*/
+test ('render overview infos', () => {
+    expect(EventWrapper.find('.title','.date','.time','.city')).toHaveLength(1)
+})
 test('render correct summary as title', () => {
     expect(EventWrapper.find('.title').text()).toBe(event.summary);
 });
-test ('render overview infos', () => {
-    expect(EventWrapper.find('.date','.time','.city')).toHaveLength(1)
-})
 test('render correct date', () => {
     expect(EventWrapper.find('.date').text()).toBe(event.start.dateTime);
 });
@@ -37,13 +35,19 @@ test('render details button', () => {
 })
 test('renders collapsed state as default', () => {
     expect(EventWrapper.state('collapsed')).toBe(true);
+    expect(EventWrapper.find('.details-btn').text()).toBe('Show details');
   });
+
+/**
+* Feature 2, Scenario 2: User can expand an event to see its details
+*/
 test('change state when details-button clicked', () => {
     EventWrapper.setState({
         collapsed:'true'
     });
     EventWrapper.find('.details-btn').simulate('click');
         expect(EventWrapper.state('collapsed')).toBe(false);
+        expect(EventWrapper.find('.details-btn').text()).toBe('Hide details');
+        expect(EventWrapper.find('.event-details')).toHaveLength(1);
 })
-
 })
