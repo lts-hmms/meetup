2ed3fabdb498c871a7ca0cdcf9cbbedd0c58e5e6
lts-hmms <97221909+lts-hmms@users.noreply.github.com>
2022-12-12 10:03:12 +0100

    feat: add tests for feature 2

--- src/Event.js
+++ src/Event.js
@@ -1,8 +1,17 @@
 import React, { Component } from "react";
 
+
 class Event extends Component {
     render(){
-        return <div></div>
+        const { event } = this.props
+        return <div>
+            <p className='title'>Learn JavaScript</p>
+            <p className='date'>2020-05-19T16:00:00+02:00</p>
+            <p className='time'>Europe/Berlin</p>
+            <p className='place'>London, UK</p>
+            <button type='button' className='details-btn'>show details</button>
+
+        </div>
     }
 }
 
---
+++ src/__tests__/Event.test.js
@@ -0,0 +1,42 @@
+
+import React from 'react';
+import { shallow } from 'enzyme';
+import Event from '../Event';
+import { mockData } from '../mock-data';
+
+describe('<Event /> component', () => {
+    let EventWrapper;
+    let event = mockData[0]
+    beforeAll(() => {
+        EventWrapper = shallow(<Event/>)
+    });
+
+    /**
+     * Feature 2, Scenario 1: An event element is collapsed by default
+     */
+test('render event title', () => {
+    expect(EventWrapper.find('.title')).toHaveLength(1);
+});
+test('render correct summary as title', () => {
+    expect(EventWrapper.find('.title').text()).toBe(event.summary);
+});
+test ('render overview infos', () => {
+    expect(EventWrapper.find('.date','.time','.city')).toHaveLength(1)
+})
+test('render correct date', () => {
+    expect(EventWrapper.find('.date').text()).toBe(event.start.dateTime);
+});
+test('render correct timezone', () => {
+    expect(EventWrapper.find('.time').text()).toBe(event.start.timeZone);
+});
+test('render correct place', () => {
+    expect(EventWrapper.find('.place').text()).toBe(event.location);
+});
+test('render details button', () => {
+    expect(EventWrapper.find('.details-btn').type()).toEqual('button');
+})
+test('renders collapsed state as default', () => {
+    expect(EventWrapper.state('collapsed')).toBe(true);
+  });
+
+})
