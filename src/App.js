ba0f198dd6ae1703a4833658aab8db6e8343794f
lts-hmms <97221909+lts-hmms@users.noreply.github.com>
2022-12-13 17:27:25 +0100

    feat: add updateEvents when select city or all

--- src/App.js
+++ src/App.js
@@ -3,16 +3,41 @@ import './App.css';
 import EventList from './EventList';
 import CitySearch from './CitySearch';
 import NumberOfEvents from './NumberOfEvents';
-import { mockData } from './mock-data';
+import { extractLocations, getEvents } from './api';
 
 class App extends Component {
+  state = {
+    events : [],
+    locations: []
+  }
+
+  componentDidMount(){
+    this.mounted = true;
+    getEvents().then((events) => {
+      this.setState({ events,locations: extractLocations(events)
+      });
+    });
+  }
+
+  componentWillUnmount(){
+    this.mounted = false;
+  }
+  
+  updateEvents = (location) => {
+    getEvents().then((events) => {
+      const locationEvents = (location === 'all') ? 
+      events: events.filter((event) => event.location === location);
+      this.setState({
+        events: locationEvents
+      });
+    });
+  }
  
   render(){
-    const events = this.state;
     return (
       <div className="App">
-        <CitySearch />
-        <EventList events ={mockData}/>
+        <CitySearch locations={this.state.locations} updateEvents={this.updateEvents}/>
+        <EventList events ={this.state.events}/>
         <NumberOfEvents />
       </div>
     );
