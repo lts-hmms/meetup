98521c6b76df2ae212d96f55aca1c9b6679179d7
lts-hmms <97221909+lts-hmms@users.noreply.github.com>
2022-12-20 12:03:44 +0100

    fix: update state only if this.mounted is true

--- src/App.js
+++ src/App.js
@@ -14,8 +14,10 @@ class App extends Component {
   componentDidMount(){
     this.mounted = true;
     getEvents().then((events) => {
+      if (this.mounted) {
       this.setState({ events,locations: extractLocations(events)
       });
+    }
     });
   }
 
