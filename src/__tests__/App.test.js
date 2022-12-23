c1b5b77146ad7d27149264bbf4627a7f946953f1
lts-hmms <97221909+lts-hmms@users.noreply.github.com>
2022-12-23 14:51:40 +0100

    feat: add test pass loc as props to citysearch

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -33,6 +33,13 @@ describe('<App /> integration', () => {
         expect(AppWrapper.find(EventList).props().events).toEqual(AppEventsState);
         AppWrapper.unmount();
     });
+    test('App passes "locations" state as a prop to CitySearch', () => {
+        const AppWrapper = mount(<App />);
+        const AppLocationsState = AppWrapper.state('locations');
+        expect(AppLocationsState).not.toEqual(undefined);
+        expect(AppWrapper.find(CitySearch).props().locations).toEqual(AppLocationsState);
+        AppWrapper.unmount();
+})
     test('get list of events matching the city selected by the user', async() => {
         const AppWrapper = mount(<App />);
         const CitySearchWrapper = AppWrapper.find(CitySearch);
