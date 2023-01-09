d870686192bc6ed963539ec3d037aea237c59f06
lts-hmms <97221909+lts-hmms@users.noreply.github.com>
2023-01-09 16:23:15 +0100

    styling events

--- src/Event.js
+++ src/Event.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import moment from "moment/moment";
 
 class Event extends Component {
     state = { collapsed: true }
@@ -12,20 +13,16 @@ class Event extends Component {
         const { event } = this.props;
         const { collapsed } = this.state;
         
-        return (<div className='event-overview'>
-            <p className='title'>{event.summary}</p>
-            <p className='date'>{new Date(event.start.dateTime).toString()}</p>
-            <p className='place'>{event.location}</p>
-            
+        return (<div className='event-overview bg-white text-center p-8 shadow-[10px_10px_5px_0px_rgba(192,132,252)] '>
+            <h2 className='title mb-2 text-xl sm:text-2xl font-semibold'>{event.summary}</h2>
+            <p className='date text-lg sm:text-xl'>{moment(event.start.dateTime).format('MMMM Do YYYY, h:mm a')}</p>
+            <p className='place text-lg sm:text-xl'>{event.location}</p>
             {!collapsed && (
-                <div className='event-details'>
-                <p className='title'>{event.summary}</p>
-                <p className='date'>{new Date(event.start.dateTime).toString()}</p>
-                <p className='place'>{event.location}</p>
+                <div className='event-details mt-6 text-lg sm:text-xl'>
                 <div className='description'>{event.description}</div>
               </div >
             )}
-            <button type='button' className='details-btn' onClick={() => this.toggleDetails()}>
+            <button type='button' className='details-btn mt-6 text-lg sm:text-xl hover:bg-purple-400 active:bg-purple-500 text-black p-3 w-48 rounded-xl border border-solid border-slate-900' onClick={() => this.toggleDetails()}>
                     {collapsed ? 'Show details' : 'Hide details'}
             </button>
 
