808f9657e9b369b27d15bdaa64caccf026933de5
lts-hmms <97221909+lts-hmms@users.noreply.github.com>
2023-01-09 16:22:56 +0100

    styling EventList

--- src/EventList.js
+++ src/EventList.js
@@ -5,7 +5,7 @@ class EventList extends Component {
     render(){
         const { events } = this.props;
         return (
-            <ul className='EventList'>
+            <ul className='EventList list-none mx-auto my-12 flex flex-col sm:flex-row items-center gap-8'>
                 {events.map(event => 
                     <li key={event.id}>
                        <Event event={event} /> 
