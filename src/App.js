251cd207a37b4fa3908b2127b969b7313c8830a3
lts-hmms <97221909+lts-hmms@users.noreply.github.com>
2023-01-09 16:24:38 +0100

    App styling

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ class App extends Component {
 
   updateEvents = (location, eventCount) => {
     getEvents().then((events) => {
-      const locationEvents = location === 'all' ? 
+      const locationEvents = location === 'all cities' ? 
       events : events.filter((event) => event.location === location);
       this.setState({
         events: locationEvents.slice(0, this.state.numOfEvents)
@@ -46,12 +46,31 @@ class App extends Component {
   
   render(){
     return (
-      <div className="App bg-gray-200 rounded-xl shadow border p-8 m-10">
+      <div className="App  bg-orange-50">
+        <div className="max-w-5xl mx-auto">
+        <section className=' my-12 p-6 flex flex-col items-center justify-center gap-8'>
+        <h2
+            className="text-center text-4xl font-bold text-slate-900 sm:text-5xl"
+          >
+            MeetUp
+        </h2>
+        <p
+            className="max-w-md text-center text-2xl text-purple-500 sm:text-3xl"
+          >
+            Discover great events in your favorite city!
+          </p>
+          </section>
+          <section className='search mb-6 p-6 flex flex-col justify-center sm:text-xl sm:flex-row gap-5 '>
         <CitySearch locations={this.state.locations} updateEvents={this.updateEvents}/>
-        <EventList events ={this.state.events} />
+        
         <NumberOfEvents num={this.state.numOfEvents} updateNumOfEvents={(num) =>
               this.updateNumOfEvents(num)}
           />
+          </section>
+          <section className='my-12 p-6 flex items-center justify-center gap-8 sm:flex-row'>
+          <EventList events ={this.state.events} />
+          </section>
+          </div>
       </div>
     );
   }
