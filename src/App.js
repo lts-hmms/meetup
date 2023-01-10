b21d89efe20a9b86b300332a513399cf5b244620
lts-hmms <97221909+lts-hmms@users.noreply.github.com>
2023-01-10 11:07:06 +0100

    code styling

--- src/App.js
+++ src/App.js
@@ -46,31 +46,30 @@ class App extends Component {
   
   render(){
     return (
-      <div className="App  bg-orange-50">
+      <div className="App bg-orange-50">
         <div className="max-w-5xl mx-auto">
-        <section className=' my-12 p-6 flex flex-col items-center justify-center gap-8'>
-        <h2
-            className="text-center text-4xl font-bold text-slate-900 sm:text-5xl"
+          <section className=' my-12 p-6 flex flex-col items-center justify-center gap-8'>
+          <h2
+              className="text-center text-4xl font-bold text-slate-900 sm:text-5xl"
           >
-            MeetUp
-        </h2>
-        <p
-            className="max-w-md text-center text-2xl text-purple-500 sm:text-3xl"
+              MeetUp
+          </h2>
+          <p
+              className="max-w-md text-center text-2xl text-purple-500 sm:text-3xl"
           >
-            Discover great events in your favorite city!
+              Discover great events in your favorite city!
           </p>
           </section>
           <section className='search mb-6 p-6 flex flex-col items-center justify-center sm:text-xl sm:flex-row gap-5 '>
-        <CitySearch locations={this.state.locations} updateEvents={this.updateEvents}/>
-        
-        <NumberOfEvents num={this.state.numOfEvents} updateNumOfEvents={(num) =>
-              this.updateNumOfEvents(num)}
-          />
+            <CitySearch locations={this.state.locations} updateEvents={this.updateEvents}/>
+            <NumberOfEvents num={this.state.numOfEvents} updateNumOfEvents={(num) =>
+                this.updateNumOfEvents(num)}
+            />
           </section>
-          <section className='my-12 p-6 flex items-center justify-center gap-8 sm:flex-col'>
-          <EventList events ={this.state.events} />
+          <section className='my-12 p-6 flex flex-col items-center justify-center gap-8 flex-col'>
+            <EventList events ={this.state.events} />
           </section>
-          </div>
+        </div>
       </div>
     );
   }
