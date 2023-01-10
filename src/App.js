3a0d06eea51b71c736d517f467b97647aac4b0e1
lts-hmms <97221909+lts-hmms@users.noreply.github.com>
2023-01-10 12:46:36 +0100

    adjustments style eventlist

--- src/App.js
+++ src/App.js
@@ -66,7 +66,7 @@ class App extends Component {
                 this.updateNumOfEvents(num)}
             />
           </section>
-          <section className='p-6 flex flex-col items-center justify-center'>
+          <section className='p-6 block items-center justify-center'>
             <EventList events ={this.state.events} />
           </section>
         </div>
