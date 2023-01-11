2cf1ad4d3506b6022975d9b2e4b65e6764d72c8b
lts-hmms <97221909+lts-hmms@users.noreply.github.com>
2023-01-11 20:31:06 +0100

    feat: abbrev. month in events

--- src/Event.js
+++ src/Event.js
@@ -15,7 +15,7 @@ class Event extends Component {
         
         return (<div className='event-overview bg-white text-center p-8 shadow-[10px_10px_5px_0px_rgba(192,132,252)] '>
             <h2 className='title mb-2 text-xl sm:text-2xl font-semibold'>{event.summary}</h2>
-            <p className='date text-lg sm:text-xl'>{moment(event.start.dateTime).format('MMMM Do YYYY, h:mm a')}</p>
+            <p className='date text-lg sm:text-xl'>{moment(event.start.dateTime).format('MMM Do YYYY, h:mm a')}</p>
             <p className='place text-lg sm:text-xl'>{event.location}</p>
             {!collapsed && (
                 <div className='event-details mt-6 text-lg sm:text-xl'>
