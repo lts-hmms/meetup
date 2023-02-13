e23a30c5ca1ede2cc7d681c1bcbef0cae026b8fe
lts-hmms <97221909+lts-hmms@users.noreply.github.com>
2023-02-13 17:17:51 +0100

    build: remove atatus

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
-import * as atatus from 'atatus-spa';
 
 // const root = ReactDOM.createRoot(document.getElementById('root'));
 ReactDOM.render(
