4ae1363f19bcaaf433e4c1df7e89ec8e55593151
lts-hmms <97221909+lts-hmms@users.noreply.github.com>
2023-02-16 17:49:01 +0100

    feat: add headlines

--- src/App.js
+++ src/App.js
@@ -81,24 +81,25 @@ class App extends Component {
       <div className="App">
         <div className="max-w-5xl mx-auto">
           <div className=' my-12 p-6 flex flex-col items-center justify-center gap-8'>
-          <h2
-              className="text-center text-4xl font-bold text-slate-900 sm:text-5xl"
+          <h1
+              className="text-4xl mb-4 text-center font-bold  sm:text-5xl"
           >
               MeetUp
-          </h2>
+          </h1>
           <p
               className="max-w-md text-center text-2xl text-purple-500 sm:text-3xl"
           >
               Discover great events in your favorite city!
           </p>
           </div>
-          <div className='search p-6 flex flex-col items-center justify-center sm:text-xl sm:flex-row gap-5 '>
+          <div className='search mb-10 p-6 flex flex-wrap justify-center sm:text-xl sm:flex-row gap-5 w-full'>
           <ErrorAlert message={warningMessage} />
             <NumberOfEvents num={numOfEvents} updateNumOfEvents={(num) =>
                 this.updateNumOfEvents(num)}
             />
             <CitySearch locations={locations} updateCityEvents={this.updateCityEvents}/>
           </div>
+          <h2 className="text-3xl mt-14 mb-8 text-center sm:text-4xl font-bold text-slate-900 ">Charts</h2>
           <div className='data-vis-wrapper'>
             <EventGenre events={events} />
             <ResponsiveContainer height={400} >
@@ -119,6 +120,7 @@ class App extends Component {
               </ScatterChart>
             </ResponsiveContainer>
           </div>
+          <h2 className="text-3xl mt-28 text-center sm:text-4xl font-bold text-slate-900 ">Events</h2>
           <div className='p-6 items-center justify-center'>
             <EventList events ={events} numOfEvents={numOfEvents}/>
           </div>
