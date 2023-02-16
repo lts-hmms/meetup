a61d3ec968d1cf79d88c536b451c4a68fe5df98f
lts-hmms <97221909+lts-hmms@users.noreply.github.com>
2023-02-16 18:51:28 +0100

    bug: remove split in filter function

--- src/eventGenre.js
+++ src/eventGenre.js
@@ -11,7 +11,7 @@ useEffect(() => {
     const getData = () => {
         const genres = ['React', 'JavaScript', 'Node', 'jQuery', 'Angular'];
         const data = genres.map((genre) => {
-            const value = events.filter((event) => event.summary.split(' ').includes(genre)).length;
+            const value = events.filter((event) => event.summary.includes(genre)).length;
             return { name: genre, value}
         })
         return data;
