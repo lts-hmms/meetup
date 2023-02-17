91b458198b263f624d3557992a198640de7161d8
lts-hmms <97221909+lts-hmms@users.noreply.github.com>
2023-02-17 10:43:46 +0100

    feat: styling welcomeScreen

--- src/WelcomeScreen.jsx
+++ src/WelcomeScreen.jsx
@@ -4,21 +4,21 @@ import './WelcomeScreen.css';
 function WelcomeScreen(props) {
     return props.showWelcomeScreen ?
     (
-        <div className="WelcomeScreen">
-            <h1>Welcome to the MeetUp app</h1>
-            <h4>Log in to see upcoming events around the world for full-stack developers</h4>
+        <div className="WelcomeScreen fixed w-full top-0 min-h-screen flex items-center text-center flex-col py-10 gap-6 bg-orange-50">
+            <h1 className="text-3xl sm:text-4xl text-purple-500 mt-36">Welcome to the MeetUp app</h1>
+            <h4 className="text-2xl sm:text-2xl">Log in to see upcoming events around the world for full-stack developers</h4>
             <div className="button_cont" align="center">
-                <div class="google-btn">
-                    <div class="google-icon-wrapper">
+                <div className="google-btn">
+                    <div className="google-icon-wrapper">
                         <img
-                            class="google-icon"
+                            className="google-icon"
                             src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
                             alt="Google sign-in"
                             />
                     </div>
                     <button onClick={() => { props.getAccessToken() }}
                             rel="nofollow noopener"
-                            class="btn-text"
+                            className="btn-text"
                     >
                         <b>Sign in with google</b>
                     </button>
