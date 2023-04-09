4ea70d43555db9dcc5d197467dc1b499d021ea44
lts-hmms <97221909+lts-hmms@users.noreply.github.com>
2023-04-09 12:35:24 +0200

    feat: center button content

--- src/WelcomeScreen.jsx
+++ src/WelcomeScreen.jsx
@@ -7,17 +7,17 @@ function WelcomeScreen(props) {
         <div className="WelcomeScreen fixed w-full top-0 min-h-screen flex items-center text-center flex-col py-10 gap-6 bg-orange-50">
             <h1 className="text-3xl sm:text-4xl text-purple-500 mt-36">Welcome to the MeetUp app</h1>
             <h4 className="text-2xl sm:text-2xl">Log in to see upcoming events around the world for full-stack developers</h4>
-            <div className="button_cont" align-content="center">
+            <div className="button_cont flex flex-col items-center">
                     <button onClick={() => { props.getAccessToken() }}
                         rel="nofollow noopener"
-                        className="btn-text"
+                        className="btn"
                     >
                         <img
-                        className="google-icon w-8"
+                        className="google-icon w-8 "
                         src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
                         alt="Google sign-in"
                         />
-                        <b>Sign in with google</b>
+                        <p>Sign in with google</p>
                     </button>
             </div>
             <a href="https://lts-hmms.github.io/meetup/privacy.html" rel="nofollow noopener">
