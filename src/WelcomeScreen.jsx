cbfaceae2e8afa1abc10e4585fd3924b3f63267f
lts-hmms <97221909+lts-hmms@users.noreply.github.com>
2023-04-09 12:17:35 +0200

    feat: center button content

--- src/WelcomeScreen.jsx
+++ src/WelcomeScreen.jsx
@@ -8,24 +8,17 @@ function WelcomeScreen(props) {
             <h1 className="text-3xl sm:text-4xl text-purple-500 mt-36">Welcome to the MeetUp app</h1>
             <h4 className="text-2xl sm:text-2xl">Log in to see upcoming events around the world for full-stack developers</h4>
             <div className="button_cont" align="center">
-                <div className="google-btn">
-                    <div className="google-icon-wrapper">
-                        <button onClick={() => { props.getAccessToken() }}
-                            rel="nofollow noopener"
-                            className="btn-text"
-                        >
-                            <img
-                            className="google-icon w-8"
-                            src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
-                            alt="Google sign-in"
-                            />
-                            <b>Sign in with google</b>
-                        </button>
-                    </div>
-                    
-                        
-                    
-                </div>
+                    <button onClick={() => { props.getAccessToken() }}
+                        rel="nofollow noopener"
+                        className="btn-text"
+                    >
+                        <img
+                        className="google-icon w-8"
+                        src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
+                        alt="Google sign-in"
+                        />
+                        <b>Sign in with google</b>
+                    </button>
             </div>
             <a href="https://lts-hmms.github.io/meetup/privacy.html" rel="nofollow noopener">
                 Privacy policy
