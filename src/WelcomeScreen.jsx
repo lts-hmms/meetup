52f66e4f70697a1baeee6528db3af19c0c2abafc
lts-hmms <97221909+lts-hmms@users.noreply.github.com>
2023-04-09 12:48:56 +0200

    feat: center google icon

--- src/WelcomeScreen.jsx
+++ src/WelcomeScreen.jsx
@@ -6,13 +6,13 @@ function WelcomeScreen(props) {
         <div className="WelcomeScreen fixed w-full top-0 min-h-screen flex items-center text-center flex-col py-10 gap-6 bg-orange-50">
             <h1 className="text-3xl sm:text-4xl text-purple-500 mt-36">Welcome to the MeetUp app</h1>
             <h4 className="text-2xl sm:text-2xl">Log in to see upcoming events around the world for full-stack developers</h4>
-            <div className="button_cont">
+            <div className="button_cont flex items-center justify-center">
                     <button onClick={() => { props.getAccessToken() }}
                         rel="nofollow noopener"
                         className="btn"
                     >
                         <img
-                        className="google-icon w-8 flex items-center justify-center"
+                        className="google-icon w-8"
                         src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
                         alt="Google sign-in"
                         />
