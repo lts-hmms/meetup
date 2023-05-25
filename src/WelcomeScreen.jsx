0b2944549e3c18c7aa3b32b8a23436860d29bd34
lts-hmms <97221909+lts-hmms@users.noreply.github.com>
2023-05-25 18:13:56 +0200

    feat: add logo and text for verification

--- src/WelcomeScreen.jsx
+++ src/WelcomeScreen.jsx
@@ -4,8 +4,13 @@ function WelcomeScreen(props) {
     return props.showWelcomeScreen ?
     (
         <div className="WelcomeScreen fixed w-full top-0 min-h-screen flex items-center text-center flex-col py-10 gap-6 bg-orange-50">
+            <img
+            className="meetupLogo"
+            src="../public/google900557fa0093f4ee.html"
+            alt="app Logo"/>
+            
             <h1 className="text-3xl sm:text-4xl text-purple-500 mt-36">Welcome to the MeetUp app</h1>
-            <h4 className="text-2xl sm:text-2xl">Log in to see upcoming events around the world for full-stack developers</h4>
+            <h4 className="text-2xl sm:text-2xl">This is an app to see upcoming events around the world for full-stack developers. Because the app uses an external google calendar to show the events, google insists on verifying all users for security reasons. This seems worrying but I assure you the app does not read or write your data in any way. </h4>
             <div className="button_cont">
                     <button onClick={() => { props.getAccessToken() }}
                         rel="nofollow noopener"
