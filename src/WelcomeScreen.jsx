eec1334446b96e5a9e9f61c065364d66a2edc2f0
lts-hmms <97221909+lts-hmms@users.noreply.github.com>
2023-05-25 18:35:20 +0200

    fix: change logo path

--- src/WelcomeScreen.jsx
+++ src/WelcomeScreen.jsx
@@ -1,16 +1,17 @@
 import React from "react";
+import logo from "../public/meetUp-app-192.png";
 
 function WelcomeScreen(props) {
     return props.showWelcomeScreen ?
     (
-        <div className="WelcomeScreen fixed w-full top-0 min-h-screen flex items-center text-center flex-col py-10 gap-6 bg-orange-50">
+        <div className="WelcomeScreen fixed w-10/12 top-0 min-h-screen flex items-center text-center flex-col py-10 gap-6 bg-orange-50">
             <img
             className="meetupLogo"
-            src="../public/meetUp-app-512.png"
-            alt="app Logo"/>
+            src={logo}
+            alt="App Logo"/>
             
             <h1 className="text-3xl sm:text-4xl text-purple-500 mt-36">Welcome to the MeetUp app</h1>
-            <h4 className="text-2xl sm:text-2xl">This is an app to see upcoming events around the world for full-stack developers. Because the app uses an external google calendar to show the events, google insists on verifying all users for security reasons. This seems worrying but I assure you the app does not read or write your data in any way. </h4>
+            <h4 className="text-2xl sm:text-2xl">This is an app to see upcoming events around the world for full-stack developers. Because the app uses an external google calendar to show the events, google insists on verifying all users for security reasons. This seems worrying but I assure you the app does not read or write your data in any way at any time. </h4>
             <div className="button_cont">
                     <button onClick={() => { props.getAccessToken() }}
                         rel="nofollow noopener"
@@ -21,7 +22,7 @@ function WelcomeScreen(props) {
                         src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
                         alt="Google sign-in"
                         />
-                        <b>Sign in with google</b>
+                        <b>Please verify with google</b>
                     </button>
             </div>
             <a href="https://lts-hmms.github.io/meetup/privacy.html" rel="nofollow noopener">
