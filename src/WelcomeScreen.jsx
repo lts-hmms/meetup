4b4ee7e15847c7f16a69bec1edc95f89eb9f6c6c
lts-hmms <97221909+lts-hmms@users.noreply.github.com>
2023-05-25 19:55:43 +0200

    fix: container content welcome screen

--- src/WelcomeScreen.jsx
+++ src/WelcomeScreen.jsx
@@ -3,8 +3,8 @@ import React from "react";
 function WelcomeScreen(props) {
     return props.showWelcomeScreen ?
     (
-        <div className="WelcomeScreen fixed w-max top-0 min-h-screen flex text-center flex-col py-10 gap-6 bg-orange-50">
-            <div className="container mx-20 items-center">
+        <div className="WelcomeScreen fixed w-max top-0 min-h-screen flex text-center flex-col py-10 bg-orange-50">
+            <div className="container mx-20 gap-6 items-center">
             <img
             className="meetupLogo"
             src="./meetup_logo.png"
