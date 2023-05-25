af46175af27e531668981499718e5843337e245e
lts-hmms <97221909+lts-hmms@users.noreply.github.com>
2023-05-25 19:18:38 +0200

    fix: changes logo and text on welcome screen

--- src/WelcomeScreen.jsx
+++ src/WelcomeScreen.jsx
@@ -3,13 +3,13 @@ import React from "react";
 function WelcomeScreen(props) {
     return props.showWelcomeScreen ?
     (
-        <div className="WelcomeScreen fixed mx-3 top-0 min-h-screen flex items-center text-center flex-col py-10 gap-6 bg-orange-50">
+        <div className="WelcomeScreen fixed mx-20 top-0 min-h-screen flex items-center text-center flex-col py-10 gap-6 bg-orange-50">
             <img
             className="meetupLogo"
-            src="./meetUp-app-192.png"
+            src="./meetUp-app-512.png"
             alt="App Logo"/>
             
-            <h1 className="text-3xl sm:text-4xl text-purple-500 mt-36">Welcome to the MeetUp app</h1>
+            <h1 className="text-3xl sm:text-4xl text-purple-500">Welcome to the MeetUp app</h1>
             <h4 className="text-2xl sm:text-2xl">This is an app to see upcoming events around the world for full-stack developers. Because the app uses an external google calendar to show the events, google insists on verifying all users for security reasons. This seems worrying but I assure you the app does not read or write your data in any way at any time. </h4>
             <div className="button_cont">
                     <button onClick={() => { props.getAccessToken() }}
